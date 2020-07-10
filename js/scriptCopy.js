37b412a6618055d9cea9858b932b41d0ef3956ea
Angel Salvador C <Angel.Gabriel.Castaneda@ibm.com>
2020-07-09 21:22:14 -0500

    responsive, script to copy done

--- js/scriptCopy.js
+++ js/scriptCopy.js
@@ -1,1 +1,21 @@
-const buttons = document.getElementsByTagName('button')
+const copyButtons = document.getElementsByClassName('copy');
+
+Object.keys(copyButtons).forEach(function(key) {
+    copyButtons[key].addEventListener('click',copy);
+});
+
+function copy(){
+    const html = this.parentNode.getElementsByClassName('codeToCopy')[0].innerHTML;
+    var doc = new DOMParser().parseFromString(html, 'text/html');
+    var text = doc.body.textContent;
+    executeCopy(text);    
+}
+
+function executeCopy(text){
+    var input = document.createElement('textarea');
+    document.body.appendChild(input);
+    input.value = text;
+    input.select();
+    document.execCommand('Copy');
+    input.remove();
+}
